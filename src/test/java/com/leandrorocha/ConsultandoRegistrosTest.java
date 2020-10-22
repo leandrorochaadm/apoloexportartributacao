025ed9116b0437c6d60505786ad69395410d04f5
leandrorochaadm <leandrorochaadm@gmail.com>
2020-10-22 18:58:56 -0400

    Java 11 no pow

---
+++ src/test/java/com/leandrorocha/ConsultandoRegistrosTest.java
@@ -0,0 +1,13 @@
+package com.leandrorocha;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+public class ConsultandoRegistrosTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	
+
+
+}
